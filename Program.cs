ca41c60ac3bdc84c023887bc19b84fe73696e280
James Pusateri <james.pusateri@middlecoastsoftware.com>
2017-07-12 20:19:02 -0500

    sample code first upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldService
+{
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        // GET hello
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, World!";
+        }
+
+        // GET hello/name
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return $"Hello, {name}!";
+        }        
+    }
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
